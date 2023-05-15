af63a828e75b073e3e2f7c8f9a184ad792f127e1
ClaireRuysschaert <netclaire@hotmail.fr>
2023-05-15 23:27:16 +0200

    refactor: get all the book informations in a function for readability

--- scrapper.py
+++ scrapper.py
@@ -1,52 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
-import bs4 
 
 url = "http://books.toscrape.com/catalogue/chase-me-paris-nights-2_977/index.html"
 
-response = requests.get(url)
-content = response.content
-if response.ok:  # returns status code 200
-    soup = BeautifulSoup(content.decode('utf-8','ignore'), features="lxml")
 
-title = ((soup.find("title")).text)[5:-1] # Remove \n and spaces from title
+def get_book_informations(url: str):
+    response = requests.get(url)
+    content = response.content
+    if response.ok:  # returns status code 200
+        soup = BeautifulSoup(content.decode("utf-8", "ignore"), features="lxml")
 
-tds = soup.findAll("td")  # column Product information with all the informations
-
-def get_the_individual_td(tds: bs4.element.ResultSet):
-    """
-    take a bs4 element ResultSet with all the rows of the page and returns text with wanted informations 
-    """
+    title = ((soup.find("title")).text)[5:-1]  # Remove \n and spaces from title
+    tds = soup.findAll("td")  # column Product information with all the informations
     upc = (tds[0]).text
-    product_type = (tds[1]).text
-    price_without_tax = ((tds[2]).text)
-    price_with_tax = ((tds[3]).text)
+    price_without_tax = (tds[2]).text
+    price_with_tax = (tds[3]).text
     availability = (tds[5]).text
-    return upc, product_type, price_without_tax, price_with_tax, availability
+    all_p_tags = soup.findAll("p")
+    description = (all_p_tags[3]).text
+    all_a_tags = soup.findAll("a")
+    category = (all_a_tags[3]).text
+    number_of_stars = soup.select_one(".star-rating").attrs["class"][1]
+    review_rating = f"{number_of_stars} out of Five"
+    image_tag = soup.select("img")
+    image_url = "http://books.toscrape.com/" + image_tag[0].get("src")
 
-upc, product_type, price_without_tax, price_with_tax, availability = get_the_individual_td(tds)
-all_p_tags = soup.findAll("p")
-description = (all_p_tags[3]).text
-all_a_tags = soup.findAll("a")
-category = (all_a_tags[3]).text
-
-def get_number_stars(selected_html: bs4.element.Tag):
-    """
-    Give the book's star rating
-    Maximum rate is 5/5
-    """
-    number_of_stars = selected_html.attrs["class"][1]
-    return f"{number_of_stars} out of Five"
+    return (
+        upc,
+        title,
+        price_with_tax,
+        price_without_tax,
+        availability,
+        description,
+        category,
+        review_rating,
+        image_url,
+    )
 
 
-number_of_stars = get_number_stars(soup.select_one(".star-rating"))
+(
+    upc,
+    title,
+    price_with_tax,
+    price_without_tax,
+    availability,
+    description,
+    category,
+    review_rating,
+    image_url,
+) = get_book_informations(url)
 
-image_tag = soup.select("img")
-image_url = "http://books.toscrape.com/" + image_tag[0].get("src")
-
-with open("test.csv", "w", encoding="utf-8-sig") as csv_file: 
+with open("test.csv", "w", encoding="utf-8-sig") as csv_file:
     csv_file.write(
         "product_page_url;universal_ product_code (upc);title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url\n"
     )
-    csv_file.write(';'.join([url, upc, title, price_with_tax, price_without_tax, availability, description, category, number_of_stars, image_url])
+    csv_file.write(
+        ";".join(
+            [
+                url,
+                upc,
+                title,
+                price_with_tax,
+                price_without_tax,
+                availability,
+                description,
+                category,
+                review_rating,
+                image_url,
+            ]
+        )
     )
