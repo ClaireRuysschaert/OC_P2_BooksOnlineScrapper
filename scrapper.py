c497d8dde356992d0d769777e2449b249f54dd8d
ClaireRuysschaert <netclaire@hotmail.fr>
2023-05-22 00:09:59 +0200

    Create root and category folder. Save each book image in its category folder.

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from soup_requester import (
     get_categories_name_url,
     get_book_list_url,
@@ -5,21 +7,39 @@ from soup_requester import (
 )
 from soup_requester import HOME_PAGE_URL
 
+# 1 create root CSV_IMAGE folder
+ROOT_FOLDER_NAME = "CSV-IMAGES"
+root_folder_path = os.path.join(os.getcwd(), ROOT_FOLDER_NAME)
+if not os.path.isdir(root_folder_path):
+    os.mkdir(root_folder_path)
 
 categories_name_url = get_categories_name_url(HOME_PAGE_URL)
-books_categories_urls = {}
 
+books_categories_urls = {}
 for category_name, category_url in categories_name_url.items():
     books_categories_urls[category_name] = get_book_list_url(category_url)
 
-for category, book_links in books_categories_urls.items():
-    csv_file_name = f"{category} - Book's information from Books to Scrape.csv"
 
-    with open(csv_file_name, "w", encoding="utf-8-sig") as csv_file:
+for category, book_links in books_categories_urls.items():
+    csv_name = f"{category} - Book's information from Books to Scrape.csv"
+
+    # 2 create one folder for each category name
+    category_folder_name = category
+    category_folder_path = os.path.join(root_folder_path, category_folder_name)
+    if not os.path.isdir(category_folder_path):
+        os.mkdir(category_folder_path)
+
+    # Create CSV file
+    with open(
+        os.path.join(ROOT_FOLDER_NAME, category_folder_name, csv_name),
+        "w",
+        encoding="utf-8-sig",
+    ) as csv_file:
         csv_file.write(
             "product_page_url;universal_ product_code (upc);title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url\n"
         )
 
+        image_title_and_urls_list = []
         for book_url in book_links:
             (
                 upc,
@@ -49,3 +69,17 @@ for category, book_links in books_categories_urls.items():
                 )
             )
             csv_file.write("\n")
+            image_title_and_urls_list.append((title, image_url))
+
+    # Create the image
+    for title, image_url in image_title_and_urls_list:
+
+        image_title = title.replace("/", "-") + ".jpg"
+
+        with open(
+            os.path.join(ROOT_FOLDER_NAME, category_folder_name, image_title), "wb"
+        ) as image_file:
+            res = requests.get(image_url)
+
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
